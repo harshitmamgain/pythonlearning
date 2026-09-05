ca6ee333f883d5882eeb1e3cf79112685274b6ec
Harshit Mamgain <hmam@softchoice.com>
2026-09-04 23:06:45 -0400

    Snapple Facts - Challenge exercise 3

---
+++ codex-control-flow-ch-3-random-fact-generator.py
@@ -0,0 +1,18 @@
+# Write code below 💖
+
+from random import randint
+
+fact = randint(1,6)
+
+if fact == 1:
+  print('Flamingos turn pink by eating shrimp.')
+elif fact == 2:
+  print('Honey never goes bad.')
+elif fact == 3:
+  print('Shrimp can only swim backwards.')
+elif fact == 4:
+  print('A taste bud\'s life is about 10 days.')
+elif fact == 5:
+  print('You can\'t sneeze while sleeping.')
+else:
+  print('Tiny pocket in jeans was for watches.')
