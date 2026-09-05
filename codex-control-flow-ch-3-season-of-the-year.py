7558c4166939beb6694a89e8568c2858e5626e7d
Harshit Mamgain <hmam@softchoice.com>
2026-09-04 23:22:38 -0400

    IF/ELSE Season of the Year

---
+++ codex-control-flow-ch-3-season-of-the-year.py
@@ -0,0 +1,12 @@
+month = int(input('Enter the month numer: '))
+
+if month in [1, 2, 3]:
+  print('Winter 🌨️')
+elif month in [4, 5, 6]:
+  print('Spring 🌱')
+elif month in [7, 8, 9]:
+  print('Summer 🌻')
+elif month in [10, 11, 12]:
+  print('Autumn 🍂')
+else:
+  print('Invalid')
